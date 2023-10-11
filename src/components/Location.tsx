4043772674633b62d822583cdbe33c935048260b
JH8459 <kk_ong2233@naver.com>
2023-10-11 23:26:26 +0900

    Modify: kakao map Ver.3

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { styled } from '@stitches/react';
 import { ConfigsType } from '../configs';
 import { kakao } from './kakao';
@@ -33,22 +33,61 @@ const SubTitle = styled('p', {
   lineHeight: 1.8,
 });
 
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
 type LocationProps = {
   config: ConfigsType;
 };
 
 const Location = ({ config }: LocationProps) => {
+  const [map, setMap] = useState<any>();
+  const [marker, setMarker] = useState<any>();
+  
   const ref = useRef<HTMLSelectElement>(null);
 
   useEffect(() => {
-    const container = document.getElementById('map');
-		const options = {
-			center: new kakao.maps.LatLng(33.450701, 126.570667),
-			level: 3
-		};
+    window.kakao.maps.load(() => {
+      const container = document.getElementById("map");
+      const options = {
+        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
+      };
+      
+      setMap(new window.kakao.maps.Map(container, options));
+      setMarker(new window.kakao.maps.Marker());
+    });
+  }, []);
 
-		const map = new kakao.maps.Map(container, options);
-  }, [])
+  const getCurrentPosBtn = () => {
+    navigator.geolocation.getCurrentPosition(
+      getPosSuccess,
+      () => alert("위치 정보를 가져오는데 실패했습니다."),
+      {
+        enableHighAccuracy: true,
+        maximumAge: 30000,
+        timeout: 27000,
+      }
+    );
+  }
+
+  const getPosSuccess = (pos: GeolocationPosition) => {
+    // 현재 위치(위도, 경도) 가져온다.
+    var currentPos = new window.kakao.maps.LatLng(
+      pos.coords.latitude, // 위도
+      pos.coords.longitude // 경도
+    );
+    // 지도를 이동 시킨다.
+    map.panTo(currentPos);
+
+    // 기존 마커를 제거하고 새로운 마커를 넣는다.
+    marker.setMap(null);
+    marker.setPosition(currentPos);
+    marker.setMap(map);
+  };
 
   return (
     <Section ref={ref}>
@@ -62,6 +101,7 @@ const Location = ({ config }: LocationProps) => {
           <br />
           도보 5분 거리입니다. 
           <br />
+          <br />
           (2호선 선릉역 5번 출구에서 셔틀버스 운행)
           <br />
           <br />
@@ -70,7 +110,8 @@ const Location = ({ config }: LocationProps) => {
             src={config.locationMapImage}
             alt="Wedding Invitation Title Picutre"
           /> */}
-          <div id = "map" style={{ width: '500px', height: '500px' }}></div>
+          <div id = "map" style={{ width: isPortrait ? '90%' : '60%' , height: '400px' }}></div>
+          <div onClick={getCurrentPosBtn}>현재 위치</div>
         </SubTitle>
       </Layout>
     </Section>
