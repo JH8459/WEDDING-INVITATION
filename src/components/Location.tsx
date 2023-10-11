4c0c960e891943df17eacd0a3f5d694cff402921
JH8459 <kk_ong2233@naver.com>
2023-10-11 23:36:20 +0900

    Modify: kakao map Ver.6

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -53,7 +53,7 @@ const Location = ({ config }: LocationProps) => {
     window.kakao.maps.load(() => {
       const container = document.getElementById("map");
       const options = {
-        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+        center: new window.kakao.maps.LatLng(37.503917, 127.042969),
         level: 3,
       };
       
